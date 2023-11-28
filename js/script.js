9783477972716ce2598fbd65f0265b116e69ad78
BeatrizUlian <beatrizh.ulian@gmail.com>
2023-11-28 12:14:31 -0500

    f

--- js/script.js
+++ js/script.js
@@ -4,12 +4,26 @@
 // });
 
 // example with showing content
-document.getElementById("change-colour-desk").addEventListener("click", function() {
-    document.getElementById("model-info").style.display = "block";
-});
+// document.getElementById("change-colour-desk").addEventListener("click", function() {
+//     document.getElementById("model-info").style.display = "block";
+// });
 
 //example with multiple models
-var myModels = ["./models/desk.glb", "./models/DeskRedOak.glb"]
+// var myModels = ["./models/desk.glb", "./models/DeskRedOak.glb"]
+// var counter = 0;
+// document.getElementById("change-colour-desk").addEventListener("click", function() {
+//     counter++;
+//     if (counter==3) { 
+//         counter = 0;
+//     }
+//     document.getElementById("model-desk").src = myModels[counter];
+// });
+
+
+
+
+//example with multiple models
+var myModels = ["./models/desk.glb", "./models/DeskRedOak.glb", "./models/Chair.glb", "./models/ChairGreyLeather.glb", "./models/ChairLightBeigeLeather.glb"]
 var counter = 0;
 document.getElementById("change-colour-desk").addEventListener("click", function() {
     counter++;
@@ -18,13 +32,6 @@ document.getElementById("change-colour-desk").addEventListener("click", function
     }
     document.getElementById("model-desk").src = myModels[counter];
 });
-
-
-
-
-//example with multiple models
-var myModels = ["./models/Chair.glb", "./models/ChairGreyLeather.glb", "./models/ChairLightBeigeLeather.glb"]
-var counter = 0;
 document.getElementById("change-colour-chair").addEventListener("click", function() {
     counter++;
     if (counter==3) { 
