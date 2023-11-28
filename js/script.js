e498101a3457dbad27e64e298f6725f06867766d
BeatrizUlian <beatrizh.ulian@gmail.com>
2023-11-28 11:53:16 -0500

    f

--- js/script.js
+++ js/script.js
@@ -4,17 +4,31 @@
 // });
 
 // example with showing content
-document.getElementById("change-colour").addEventListener("click", function() {
+document.getElementById("change-colour-desk").addEventListener("click", function() {
     document.getElementById("model-info").style.display = "block";
 });
 
 //example with multiple models
 var myModels = ["./models/desk.glb", "./models/DeskRedOak.glb"]
 var counter = 0;
-document.getElementById("change-colour").addEventListener("click", function() {
+document.getElementById("change-colour-desk").addEventListener("click", function() {
     counter++;
     if (counter==3) { 
         counter = 0;
     }
-    document.getElementById("model").src = myModels[counter];
+    document.getElementById("model-desk").src = myModels[counter];
+});
+
+
+
+
+//example with multiple models
+var myModels = ["./models/Chair.glb", "./models/ChairGreyLeather.glb", "./models/ChairLightBeigeLeather.glb"]
+var counter = 0;
+document.getElementById("change-colour-chair").addEventListener("click", function() {
+    counter++;
+    if (counter==3) { 
+        counter = 0;
+    }
+    document.getElementById("model-chair").src = myModels[counter];
 });
